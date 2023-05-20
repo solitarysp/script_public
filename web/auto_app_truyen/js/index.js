51d4f2a09c97d7ca4122efe549ab8f44358845c3
solitarysp <solitarysp@gmail.com>
2023-05-20 10:51:10 +0700

    add

--- web/auto_app_truyen/js/index.js
+++ web/auto_app_truyen/js/index.js
@@ -1,4 +1,4 @@
-setInterval(elementSelectFree, 5000);
+setInterval(elementSelectFree, 1000);
 
 function elementSelectFree() {
     try {
@@ -27,7 +27,7 @@ function elementSelectFree() {
         if (document.location.href.indexOf('/app-content/content-rating-iarc-questionnaire?source=dashboard') > 0) {
             const inputs = document.getElementsByClassName('mdc-radio__native-control')
             for (let i = 0; i < inputs.length; i++) {
-                if(i%2 >0){
+                if (i % 2 > 0) {
                     document.getElementsByClassName('mdc-radio__native-control')[i].click()
                 }
             }
@@ -37,8 +37,29 @@ function elementSelectFree() {
     }
     try {
         if (document.location.href.indexOf('/app-content/target-audience-content?source=dashboard') > 0) {
-            document.getElementsByClassName('mdc-checkbox__native-control')[5].click()
-            document.getElementsByClassName('mdc-radio__native-control')[1].click()
+            if (checkTonTaiContent("span", "Độ tuổi mục tiêu")) {
+                try {
+                    var element = document.getElementsByClassName('mdc-checkbox__native-control')[5]
+                    if (!element.checked) {
+                        element.click();
+                    }
+                    ClickNext("div", "Tiếp theo")
+                } catch (e) {
+                }
+            }
+
+            if (checkTonTaiContent("span", "Sự hiện diện trong Cửa hàng Play")) {
+                try {
+                    document.getElementsByClassName('mdc-radio__native-control')[1].click()
+                } catch (e) {
+                }
+            }
+            if (checkTonTaiContent("span", "Dưới đây là những thông tin bạn đã cho chúng tôi biết")) {
+                try {
+                    ClickNext("div", "Lưu")
+                } catch (e) {
+                }
+            }
         }
     } catch (e) {
 
@@ -46,22 +67,60 @@ function elementSelectFree() {
     try {
         if (document.location.href.indexOf('/app-content/news-declaration?source=dashboard') > 0) {
             document.getElementsByClassName('mdc-radio__native-control')[0].click()
+            try {
+                ClickNext("div", "Lưu")
+            } catch (e) {
+            }
         }
     } catch (e) {
 
     }
     try {
         if (document.location.href.indexOf('/app-content/regulated-health-apps?source=dashboard') > 0) {
-            document.getElementsByClassName('mdc-checkbox__native-control')[2].click()
+            var element = document.getElementsByClassName('mdc-checkbox__native-control')[2]
+            if (!element.checked) {
+                element.click();
+            }
+            ClickNext("div", "Lưu")
+        }
+    } catch (e) {
+
+    }
+    try {
+        if (document.location.href.indexOf('/app-content/data-privacy-security?source=dashboard') > 0) {
+            ClickNext("span", "Tiếp theo")
+            document.getElementsByClassName('mdc-radio__native-control')[4].click();
+            ClickNext("span", "Tiếp theo")
+            ClickNext("div", "Lưu")
         }
     } catch (e) {
 
     }
     try {
         if (document.location.href.indexOf('/app-content/government-apps?source=dashboard') > 0) {
-            document.getElementsByClassName('mdc-radio__native-control')[1].click()
+            document.getElementsByClassName('mdc-radio__native-control')[1].click();
+            ClickNext("div", "Lưu");
         }
     } catch (e) {
 
     }
+}
+
+function ClickNext(tagName, content) {
+    const elements = document.getElementsByTagName(tagName);
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].textContent.trim() == content) {
+            elements[i].click();
+        }
+    }
+}
+
+function checkTonTaiContent(tagName, content) {
+    const elements = document.getElementsByTagName(tagName);
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].textContent.trim() == content) {
+            return true;
+        }
+    }
+    return false;
 }
